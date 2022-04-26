ecd8ce708e319deff7324352ac05a4aba622561f
RodolfoBaume <robalaz@hotmail.com>
2022-04-25 23:57:58 -0500

    Empezando refactoring

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-// Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+// // Part 1 Read json file ===========================
+// const rawdata = fs.readFileSync("explorers.json");
+// const explorers = JSON.parse(rawdata); //lista de explorers del archivo json
+
+// console.log("hey! I'm HERE!")
+// // console.log(explorers)
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+console.log("Explorers en node:" + explorersInNode.length)
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+console.log(usernamesInNode)
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
@@ -49,6 +52,7 @@ const explorersInNodeAndBuzzTrick = explorersInNode.map((explorer) => assignBuzz
 //Part7: Get a new list of explorers in Node, if the score number is divisible by 3 AND by 5 we need to set a new property called FIZZBUZZ, if not this value should be the same score value
 
 const assignFizzBuzzTrick = function(explorer){
+    console.log("Appliying this function")
     if(explorer.score%5 === 0 && explorer.score%3 === 0){
         explorer.trick = "FIZZBUZZ";
         return explorer;
@@ -59,5 +63,7 @@ const assignFizzBuzzTrick = function(explorer){
 };
 
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
+console.log(explorersInNodeAndFizzBuzzTrick)
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+
---
+++ index.js
@@ -0,0 +1,29 @@
+const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService")
+const FizzbuzzService = require("./lib/services/FizzbuzzService")
+
+const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+// console.log(explorers)
+
+
+
+// Clase anterior con la que obtenemos los explorers
+// const Reader = require("./lib/utils/Reader");
+// const explorers = Reader.readJsonFile("explorers.json");
+
+// Aplicación del ExplorerService sobre la lista de explorers
+console.log(ExplorerService.filterByMission(explorers, "node"))
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+
+const explorer1 = {name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
