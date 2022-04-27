1ced3130b66675c51ca551ff4d4172b669e114f0
RodolfoBaume <robalaz@hotmail.com>
2022-04-27 14:05:14 -0500

    Agregando ESLint y ajustando archivos con el linter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Reader = require("./lib/utils/Reader");
-const ExplorerService = require("./lib/services/ExplorerService")
-const FizzbuzzService = require("./lib/services/FizzbuzzService")
+const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
 const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
 // console.log(explorers)
@@ -12,18 +12,18 @@ const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la list
 // const explorers = Reader.readJsonFile("explorers.json");
 
 // Aplicación del ExplorerService sobre la lista de explorers
-console.log(ExplorerService.filterByMission(explorers, "node"))
-console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"))
-console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"))
+console.log(ExplorerService.filterByMission(explorers, "node"));
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
 
-const explorer1 = {name: "Explorer1", score: 1}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // {name: "Explorer1", score: 1, trick: 1} 
+const explorer1 = {name: "Explorer1", score: 1};
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)); // {name: "Explorer1", score: 1, trick: 1} 
 
-const explorer3 = {name: "Explorer3", score: 3}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+const explorer3 = {name: "Explorer3", score: 3};
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)); // {name: "Explorer3", score: 3, trick: "FIZZ"}
 
-const explorer5 = {name: "Explorer5", score: 5}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+const explorer5 = {name: "Explorer5", score: 5};
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)); // {name: "Explorer5", score: 5, trick: "BUZZ"}
 
-const explorer15 = {name: "Explorer15", score: 15}
-console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+const explorer15 = {name: "Explorer15", score: 15};
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
